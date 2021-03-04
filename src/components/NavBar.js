31f1aaa916e483413c34b82912ca7863ac9db475
goduu <idcangussu@hotmai.com>
2021-03-03 21:04:03 -0300

    Added Router

---
+++ src/components/NavBar.js
@@ -0,0 +1,33 @@
+import './NavBar.css';
+import React from 'react'
+import {Link} from "react-router-dom";
+import Button from './Button'
+class NavBar extends React.Component{
+
+    constructor(props) {
+        super(props);
+        
+    }
+
+    render() {
+        return (
+            <div className="navbac-container">
+                <ul className="navbar">
+                    <li className="nb-list-item">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="nb-list-item"> 
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li className="nb-list-item">
+                        <Link to="/showroom">ShowRoom</Link>
+                    </li>
+                    
+                </ul>
+            </div>
+        );
+            
+    }
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Router';
 import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fas)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode >
     <App />
   </React.StrictMode>,
   document.getElementById('root')
